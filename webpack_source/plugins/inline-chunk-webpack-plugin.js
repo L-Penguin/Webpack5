b4108ff187fd5bb213dc9c5e85b10f0618426d7e
q.li5 <q.li5@trip.com>
2022-11-01 12:05:34 +0800

    add: 添加plugin内容

---
+++ webpack_source/plugins/inline-chunk-webpack-plugin.js
@@ -0,0 +1,68 @@
+const HtmlWebpackPlugin = require('safe-require')('html-webpack-plugin');
+
+class InlineChunkPlugin {
+    constructor(tests) {
+        this.tests = tests;
+    }
+    apply (compiler) {
+        compiler.hooks.compilation.tap('InlineChunkPlugin', (compilation) => {
+            // 1. 获取html-webpack-plugin的hooks
+            const hooks = HtmlWebpackPlugin.getHooks(compilation)
+            // 2. 注册html-webpack-plugin的hooks -> alterAssetTagGroups
+            hooks.alterAssetTagGroups.tap(
+                'InlineChunkPlugin', 
+                (assets) => {
+                    // 3. 从里面将script的runtime文件，变成inline script
+                    assets.headTags = this.getInlineChunk(assets.headTags, compilation.assets);
+                    assets.bodyTags = this.getInlineChunk(assets.bodyTags, compilation.assets);
+                }
+            )
+            // 刪除runtime文件
+            hooks.afterEmit.tap("InlineChunkPlugin", () => {
+                Object.keys(compilation.assets).forEach(filepath => {
+                    if (this.tests.some(test => test.test(filepath))) {
+                        delete compilation.assets[filepath];
+                    }
+                })
+            })
+        })
+    }
+
+    getInlineChunk(tags, assets) {
+        /**
+         * 目前：
+         * [
+         *      {
+         *         tagName: 'script',
+         *         voidTag: false,
+         *         meta: { plugin: 'html-webpack-plugin' },
+         *         attributes: { defer: true, type: undefined, src: 'js/main.js' }
+         *      }
+         * ]
+         * 修改为：
+         * [
+         *      {
+         *          tagName: 'script',
+         *          innerHTML: runtime文件的内容
+         *          closeTag: true
+         *      }
+         *  ]
+         */
+        return tags.map(tag => {
+            if (tag.tagName !== "script" ) return tag;
+            // 获取文件资源路径
+            const filepath= tag.attributes.src;
+            if (!filepath) return tag;
+
+            if (!this.tests.some(test => test.test(filepath))) return tag;
+            
+            return {
+                tagName: "script",
+                innerHTML: assets[filepath].source(),
+                closeTag: true,
+            }
+        })
+    }
+}
+
+module.exports = InlineChunkPlugin
--- webpack_source/webpack.config.js
+++ webpack_source/webpack.config.js
@@ -1,36 +1,40 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const TestPlugin = require("./plugins/test-plugin")
+const TestPlugin = require("./plugins/test-plugin");
+const BannerWebpackPlugin = require("./plugins/banner-webpack-plugin");
+const CleanWebpackPlugin = require("./plugins/clean-webpack-plugin");
+const AnalyzeWebpackPlugin = require("./plugins/analyze-webpack-plugin");
+const InlineChunkWebpackPlugin = require("./plugins/inline-chunk-webpack-plugin");
 
 module.exports = {
     entry: "./src/main.js",
     output: {
         path: path.resolve(__dirname, "./dist"),
         filename: "js/[name].js",
-        clean: true,
+        // clean: true,
     },
     module: {
         rules: [
-            {
-                test: /\.js$/,
-                //  loader: "./loaders/test-loader.js",
-                /* use: [
-                    "./loaders/demo/test1",
-                    "./loaders/demo/test2"
-                ], */
-                // loader: "./loaders/demo/test3"
-                /* use: [
-                    "./loaders/demo/test4",
-                    "./loaders/demo/test5",
-                    "./loaders/demo/test6",
-                ], */
-                // loader: "./loaders/clean-log-loader",
-                loader: "./loaders/banner-loader",
-                options: {
-                    author: "老王",
-                    // age: 12, // 不能新增字段，不然会报错
-                }
-            },
+            // {
+            //     test: /\.js$/,
+            //     //  loader: "./loaders/test-loader.js",
+            //     /* use: [
+            //         "./loaders/demo/test1",
+            //         "./loaders/demo/test2"
+            //     ], */
+            //     // loader: "./loaders/demo/test3"
+            //     /* use: [
+            //         "./loaders/demo/test4",
+            //         "./loaders/demo/test5",
+            //         "./loaders/demo/test6",
+            //     ], */
+            //     // loader: "./loaders/clean-log-loader",
+            //     loader: "./loaders/banner-loader",
+            //     options: {
+            //         author: "老王",
+            //         // age: 12, // 不能新增字段，不然会报错
+            //     }
+            // },
             {
                 test: /\.js$/,
                 loader: "./loaders/babel-loader",
@@ -56,7 +60,23 @@ module.exports = {
         new HtmlWebpackPlugin({
             template: path.resolve(__dirname, "./public/index.html"),
         }),
-        new TestPlugin(),
+        // new TestPlugin(),
+        new BannerWebpackPlugin({
+            author: "L_PenguinQAQ"
+        }),
+        new CleanWebpackPlugin(),
+        new AnalyzeWebpackPlugin(),
+        new InlineChunkWebpackPlugin([
+            /runtime(.*)\.js$/g,
+        ]),
     ],
-    mode: "development",
+    optimization: {
+        splitChunks: {
+            chunks: "all",
+        },
+        runtimeChunk: {
+            name: entrypoint => `runtime~${entrypoint.name}.js`
+        }
+    },
+    mode: "production",
 }
